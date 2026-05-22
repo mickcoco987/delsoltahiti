1cda35a21da17a4362aec43d731571a3114877ba
mickcoco987 <mickael.cohen@gmail.com>
2026-05-22 12:50:12 -1000

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ window.COTE_CONFIG = {
   // URL publique du Worker Cloudflare qui declenche la mise a jour
   // (voir worker/update-cote-worker.js pour le deploiement).
   // Laisser vide => le bouton ouvre simplement la page GitHub Actions.
-  updateEndpoint: "",
+  updateEndpoint: "https://delsoltahiti-update.mickael-cohen.workers.dev",
 };
